e147940915e9c43bb433913af6462292af40fea1
Miguel Jiménez <migueljimenezachinte@gmail.com>
2017-08-10 15:06:04 -0700

    Add "advanced" mode in installer

---
+++ windows/run-advanced.bat
@@ -0,0 +1,24 @@
+:: run-advanced.bat
+:: Author: Miguel Jimenez
+:: Date  : August 9, 2017
+::
+:: This program selects and runs the adequate
+:: CLion+dependencies installer.
+::
+:: Arguments: --
+::
+@ECHO off
+
+:: Select installer and execute it
+SET /p choice= "Would you like to use (1) Cygwin or (2) MinGW? "
+IF %choice%==1 (
+	SET PROVIDER="CYGWIN"
+) ELSE IF %choice%==2 (
+	SET PROVIDER="MINGW"
+) ELSE (
+	ECHO The option you typed in is not valid.
+	ECHO This window will close in 5 seconds.
+	TIMEOUT /T 5 /NOBREAK
+)
+
+CMD /k ""files\install.bat" %PROVIDER%"
--- windows/run.bat
+++ windows/run.bat
@@ -1,24 +1,10 @@
 :: run.bat
 :: Author: Miguel Jimenez
-:: Date  : August 9, 2017
+:: Date  : August 10, 2017
 ::
-:: This program selects and runs the adequate
-:: CLion+dependencies installer.
+:: This program runs the CLion+MinGW installer.
 ::
 :: Arguments: --
 ::
 @ECHO off
-
-:: Select installer and execute it
-SET /p choice= "Would you like to use (1) Cygwin or (2) MinGW? "
-IF %choice%==1 (
-	SET PROVIDER="CYGWIN"
-) ELSE IF %choice%==2 (
-	SET PROVIDER="MINGW"
-) ELSE (
-	ECHO The option you typed in is not valid.
-	ECHO This window will close in 5 seconds.
-	TIMEOUT /T 5 /NOBREAK
-)
-
-CMD /k ""files\install.bat" %PROVIDER%"
+CMD /k ""files\install.bat" MINGW"
