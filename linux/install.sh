065ab150fc5f905607a735d46ce954de0783c404
Miguel Jimenez <migueljimenezachinte@gmail.com>
2017-09-05 12:56:56 -0700

    Update linux/install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -9,8 +9,8 @@
 CLION_VERSION="2017.2.1"
 CLION_FILE="CLion-$CLION_VERSION.tar.gz"
 CLION_URL="https://download-cf.jetbrains.com/cpp/$CLION_FILE"
-CLION_EXECUTABLE="/opt/CLion-$CLION_VERSION/bin/clion.sh"
-ALIAS_EXISTS_MSG="The command clion was not set because it already exists"
+CLION_EXECUTABLE="/opt/clion-$CLION_VERSION/bin/clion.sh"
+INPUT_PASSWORD_MSG="Your password may be asked in order to extract the files."
 
 echo ---------------------------------------------------------------
 echo                   University of Victoria
@@ -26,18 +26,25 @@ echo
 wget -c -O $CLION_FILE $CLION_URL
 
 echo + Extracting files
+echo -e "$INPUT_PASSWORD_MSG\n"
 sudo tar xf $CLION_FILE -C /opt/ &> /dev/null
+echo
+echo
 
 # Add alias to open the IDE
-echo + Creating alias
-alias clion && echo " $ALIAS_EXISTS_MSG" || \
-	echo "# The following alias was automatically created on $(date)" >> ~/.bash_aliases \
-	&& echo "alias clion='command'" >> ~/.bash_aliases \
-	&& source ~/.bash_aliases
+if ! command -V clion > /dev/null 2>&1; then
+	echo "# This alias was automatically created on $(date)" >> ~/.bash_aliases
+	echo "alias clion='$CLION_EXECUTABLE & disown'" >> ~/.bash_aliases
+	echo  The \'clion\' alias was created in your .bash_aliases file.
+else
+	echo The command clion was not set because it already exists.
+fi
+# Source the aliases
+. ~/.bash_aliases
 
-echo 
-echo  The "clion" alias was created in your .bash_aliases file. To
-echo  open the IDE, execute the command "clion" in a terminal.
+echo  To open the IDE, execute the command \'clion\' in a terminal. In case
+echo  the command is not recognised, execute \'source ~/.bash_aliases\' and
+echo  try again.
 echo 
 echo  You may now close this window and remove the files. Bye!
 echo ---------------------------------------------------------------
